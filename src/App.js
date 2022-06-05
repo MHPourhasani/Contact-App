939b3df7191e911e133b9ba871ab7c0f641651a6
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-06 04:16:39 +0430

    create routes.js and import HomePage in it. in App.js map on routes.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routes from './routes';
+
 function App() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				{routes.map((route) => (
+					<Route {...route} element={<route.component />} key={route.path} />
+				))}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import HomePage from './Pages/HomePage'
+
+const routes = [
+	{ path: '/', component: HomePage },
+];
+
+export default routes;
